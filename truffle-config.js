2ecb52bdb5f3e6e09ee420450daf2006298f1a07
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-09-28 21:10:31 +0200

    feat: update prism plug

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,21 @@
-module.exports = {
+
+// Initial Configuration
+const config = {
     compilers: {
         solc: {
             version: "0.5.7"
         }
     },
     plugins: ["solidity-coverage"]
-//
 };
+
+try {
+    const outter_config = require('../../prism');
+    module.exports = {
+        ...config,
+        ...outter_config
+    };
+} catch (e) {
+    module.exports = config;
+}
+
