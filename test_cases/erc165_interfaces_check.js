611f5693504a2fc499228a32db5ae529ca4051d3
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2019-11-02 12:56:49 +0100

    fix: update erc2280 signature

--- test_cases/erc165_interfaces_check.js
+++ test_cases/erc165_interfaces_check.js
@@ -3,7 +3,7 @@ module.exports = {
         const daiplus = artifacts.require('DaiPlus');
         const daiplus_instance = await daiplus.deployed();
 
-        expect(await daiplus_instance.supportsInterface('0x6941bcc3')).to.equal(true);
+        expect(await daiplus_instance.supportsInterface('0x25961920')).to.equal(true);
         expect(await daiplus_instance.supportsInterface('0x01ffc9a7')).to.equal(true);
         expect(await daiplus_instance.supportsInterface('0x36372b07')).to.equal(true);
         expect(await daiplus_instance.supportsInterface('0x06fdde03')).to.equal(true);
